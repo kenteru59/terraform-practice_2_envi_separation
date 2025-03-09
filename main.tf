00510ee4fb23bcfe230464e75c5119b619d288fd
k-abe <f.kaebnet.o.cus@gmail.com>
2025-03-09 21:45:03 +0900

    initial commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+# TerraformのstateファイルをS3に保存する
+terraform {
+  backend "s3" {
+    key            = "workspace-example/terraform.tfstate"
+  }
+}
+
+data "aws_vpc" "default" {
+  filter {
+    name   = "tag:Name"
+    values = ["default-vpc"]
+  }
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+  filter {
+    name   = "map-public-ip-on-launch"
+    values = ["true"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-0a290015b99140cd1"
+  instance_type = "t2.micro"
+  subnet_id = data.aws_subnets.default.ids[0]
+}
